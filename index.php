1d940e222600111ba402f196aa2c0b5848fabab3
anan0112 <106704235+anan0112@users.noreply.github.com>
2022-06-05 17:42:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,53 @@
-<?
-  echo "test';
-?>
+<!DOCTYPE html>
+	<head>
+		<title> TITLE </title>
+		<meta charset="utf-8">
+		<link rel = "shortcut icon" href = "image/ncu.ico" type = "image/x-icon"> <!-- 圖片來源：https://www.ncu.edu.tw/favicon.ico -->
+		<link rel = "stylesheet" href = "mystyle.css">
+	</head>
+	<body>
+		<div id="Header">
+			<img src="image/ncu.png" alt="國立中央大學" width="49%" height="160px"> <!-- 圖片來源：https://www.ncu.edu.tw/_images/all/logo.png -->
+			<img src="image/mis.png" alt="中央大學資管系" width="49%" height="160px">
+		</div>
+		
+		<div id="Leftside">
+			
+			<img class="round_img" src="image/headshot.jpg" alt="大頭照" width="200" style="display:block; margin:auto;">
+			<h1>曾安汝</h1>
+			<h4>國立中央大學資訊管理學系</h4>
+			<hr>
+			<p>我是曾安汝，目前就讀於國立中央大學資訊管理學系，同時也兼職於IKEA宜家家居桃園店。</p>
+			<p>大學第一年，我加入了中大資管系系學會學術部。在那裡，我認識了一些朋友和系上的學長姊，也學會了如何與團隊進行分工合作、上台講話以及舉辦活動的技能</p>
+			<p>第二年，為了早點適應獨立生活，我進入了IKEA擔任兼職銷售人員一職賺取生活費。在那裡，我實際體會到學校老師與上司之間的不同，以及團隊中的氣氛是如何影響整個群體的。因為工作性質的關係，我常常要與各式各樣的客人進行交談，在這之中也讓我磨練了自己的說話技巧。</p>
+			<p>在IKEA工作了一段時間後我有了一些心得，就是資料系統的好壞真的很重要，因此在這裡我要期許我自己在接下來大學剩餘的兩年能夠好好認真學習相關知識，並對自己未來的工作有所幫助。</p>
+      <?
+          echo "test';
+      ?>
+
+			
+		</div>
+		
+		<div id="Rightside">
+			<div class="data">
+				<h2 style="margin-left: 2em;">學歷</h2>
+				<hr class="hr1">
+			</div>
+			<ul>
+				<h3>Programming Language</h3>
+				<ul>
+					<li type="disc">C</li>
+					<li type="disc">Python</li>
+					<li type="disc">JAVA</li>
+					<li type="disc">HTML/CSS</li>
+				</ul>
+			</ul>
+		</div>
+		
+		<div id="Footer">
+		
+		</div>
+		
+		
+	</body>
+</html>
